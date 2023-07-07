6a30d823e890884fc9bfb57165df88b2d6bbf8a4
Stanmina <stan.vdm.2003@gmail.com>
2023-07-07 09:04:49 +0200

    screen saver

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -1,12 +1,17 @@
 export default [
-    {
-        name: "room",
-        type: "glbModel",
-        path: "/models/Portfolio.glb"
-    },
-    {
-        name: "screen",
-        type: "videoTexture",
-        path: "/textures/vid.mp4"
-    }
-]
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/Portfolio.glb",
+  },
+  {
+    name: "screen",
+    type: "videoTexture",
+    path: "/textures/portfolio.mp4",
+  },
+  {
+    name: "screen2",
+    type: "videoTexture",
+    path: "/textures/vid.mp4",
+  },
+];
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -2,68 +2,71 @@ import * as THREE from "three";
 import Experience from "../Experience";
 import GSAP from "gsap";
 
-export default class Room{
-    constructor(){
-        this.experience = new Experience();
-        this.scene = this.experience.scene;
-        this.resources = this.experience.resources;
-        this.room = this.resources.items.room;
-        this.actualRoom = this.room.scene;
+export default class Room {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.room = this.resources.items.room;
+    this.actualRoom = this.room.scene;
 
-        this.lerp = {
-            current: 0,
-            target: 0,
-            ease: 0.1
-        }
+    this.lerp = {
+      current: 0,
+      target: 0,
+      ease: 0.1,
+    };
 
-        this.setModel()
-        this.onMouseMove();
-    }
+    this.setModel();
+    this.onMouseMove();
+  }
 
-    setModel(){
-        this.actualRoom.children.forEach((child) => {
-            child.castShadow = true;
-            child.receiveShadow = true;
+  setModel() {
+    this.actualRoom.children.forEach((child) => {
+      child.castShadow = true;
+      child.receiveShadow = true;
 
-            if (child instanceof THREE.Group){
-                child.children.forEach((groupChild) => {
-                    groupChild.castShadow = true;
-                    groupChild.receiveShadow = true;
+      if (child instanceof THREE.Group) {
+        child.children.forEach((groupChild) => {
+          groupChild.castShadow = true;
+          groupChild.receiveShadow = true;
 
-                    // NTS
-                    // update names in blender
-                    if(groupChild.name === "Cube019_1" || groupChild.name === "Cube021_1"){
-                        groupChild.material = new THREE.MeshBasicMaterial({
-                            map: this.resources.items.screen,
-                        });
-                    }
-                })
-            }
+          // NTS
+          // update names in blender
+          if (
+            groupChild.name === "Cube019_1" ||
+            groupChild.name === "Cube021_1"
+          ) {
+            groupChild.material = new THREE.MeshBasicMaterial({
+              map: this.resources.items.screen,
+            });
+          }
         });
-        this.scene.add(this.actualRoom);
-        this.actualRoom.position.y = 2.5;
-        this.actualRoom.scale.set(.3, .3, .3);
-    }
+      }
+    });
+    this.scene.add(this.actualRoom);
+    this.actualRoom.position.y = 2.5;
+    this.actualRoom.scale.set(0.3, 0.3, 0.3);
+  }
 
-    onMouseMove(){
-        window.addEventListener("mousemove", (e) => {
-            this.rotation = ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
-            this.lerp.target = this.rotation * 0.1;
-            this.update();
-        });
-    }
+  onMouseMove() {
+    window.addEventListener("mousemove", (e) => {
+      this.rotation =
+        ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
+      this.lerp.target = this.rotation * 0.1;
+      this.update();
+    });
+  }
 
-    resize(){
-    }
+  resize() {}
 
-    update(){
-        // console.log("Room update()");
-        this.lerp.current = GSAP.utils.interpolate(
-            this.lerp.current,
-            this.lerp.target,
-            this.lerp.ease
-        );
+  update() {
+    // console.log("Room update()");
+    this.lerp.current = GSAP.utils.interpolate(
+      this.lerp.current,
+      this.lerp.target,
+      this.lerp.ease
+    );
 
-        this.actualRoom.rotation.y = this.lerp.current;
-    }
+    this.actualRoom.rotation.y = this.lerp.current;
+  }
 }
